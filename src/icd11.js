ba25b575d75c1f6f47528dbc912b48373f30e064
Padam Dahal <dahal0514@gmail.com>
2026-05-26 09:42:02 +0000

    code update

---
+++ src/icd11.js
@@ -0,0 +1,28 @@
+// DHIS2 IDs for each category combinations
+// var coc = {category_name:[uuid, data_column],...}
+var coc = {
+  "0to7days_female": ["fKrPh3mNwuA","C"],
+  "0to7days_male": ["AqOY5BvwChr","D"],
+  "8to28days_female": ["C1nelEDcfkS","E"],
+  "8to28days_male": ["YaaywTY9eKq","F"],
+  "29to1yr_female": ["MQj7sk1V2Wd","G"],
+  "29to1yr_male":["QQt2JrzlZbo","H"],
+  "1to4yrs_female": ["mh3DYEYBSpg","I"],
+  "1to4yrs_male": ["EkuvSkGwGeG","J"],
+  "5to14yrs_female": ["ImeluxdRw47","K"],
+  "5to14yrs_male": ["YF8d9qgfLzR","L"],
+  "15to19yrs_female": ["lo7vNzsUiBe","M"],
+  "15to19yrs_male": ["dU1fetpED4w","N"],
+  "20to29yrs_female": ["miqFgXB82SV","O"],
+  "20to29yrs_male": ["m05Jven6ovZ","P"],
+  "30to39yrs_female": ["QOM5Xn9HRRF","Q"],
+  "30to39yrs_male": ["lu1kdcpahOB","R"],
+  "40to49yrs_female": ["KpJMRZlgWu2","S"],
+  "40to49yrs_male": ["mpbNAZM7jRI","T"],
+  "50to59yrs_female": ["C9vCxjeox4h","U"],
+  "50to59yrs_male": ["XwK3g2awV0e","V"],
+  "60to69yrs_female": ["VDZFWJbjE7D","W"],
+  "60to69yrs_male": ["dhIPuVKMinp","X"],
+  "70plus_female": ["N6V9yOzGjyc","Y"],
+  "70plus_male": ["PrnAj8Z0mrJ","Z"]
+};
